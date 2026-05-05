fec242a3dc6c5fc3da9c27036990c1e214c5e1a7
vhyulumyan-boop <vhyulumyan@46.ukr.education>
2026-05-06 01:23:47 +0300

    uploading files

---
+++ script.js
@@ -0,0 +1,8 @@
+let photo = 0;
+let numOfPhoto = 3;
+
+function change() {
+    photo++;
+    let elem = document.getElementById('author-photo');
+    elem.src = `./images/photo${photo % numOfPhoto + 1}.jpg`;
+}
