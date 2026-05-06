16dacbd7097d69528359e18b3d5b2e7fc0f77e49
vhyulumyan-boop <vhyulumyan@46.ukr.education>
2026-05-06 08:44:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ let numOfPhoto = 3;
 function change() {
     photo++;
     let elem = document.getElementById('author-photo');
-    elem.src = `./images/photo${photo % numOfPhoto + 1}.jpg`;
+    elem.src = `./photo${photo % numOfPhoto + 1}.jpg`;
 }
